404acfaa535b497f379401f3ba0a89b3d70aac66
GDeath019 <duong019ds@gmail.com>
2020-04-07 15:05:56 +0700

    abc

---
+++ app/src/main/java/com/example/attendance/monthCalendar.java
@@ -0,0 +1,14 @@
+package com.example.attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class monthCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_calendar);
+    }
+}
